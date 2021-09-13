ab090519259b3b30da45a44a8834a2d1c65987d2
Gui-dev <adrianon10@gmail.com>
2021-09-13 04:15:50 -0300

    feat: add template Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import Head from 'next/head'
 
+import { HomeTemplate } from 'templates/Home'
+
 export default function Home () {
   return (
     <>
       <Head>
-        <title>Início | News</title>
+        <title>Home | News</title>
       </Head>
+
+      <HomeTemplate />
     </>
   )
 }
---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,18 @@
+import styles from './style.module.scss'
+
+export const HomeTemplate = () => {
+  return (
+    <main className={ styles.contentContainer }>
+      <section className={ styles.hero }>
+        <span>👏 Hey, Welcome</span>
+        <h1>News about<br /> the <span>React</span> world</h1>
+        <p>
+          Get access to all the publications <br />
+          <span>for $9.90 month</span>
+        </p>
+      </section>
+
+      <img src="/images/avatar.svg" alt="Girl coding on a table" />
+    </main>
+  )
+}
