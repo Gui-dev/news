c11054d66d1f6c9637d8e49043ccedf2692c581d
Gui-dev <adrianon10@gmail.com>
2021-09-14 04:11:58 -0300

    feat: add helper function getServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,46 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 
+import { stripe } from 'services/stripe'
+import { formatCurrency } from 'utils/formatCurrency'
 import { HomeTemplate } from 'templates/Home'
 
-export default function Home () {
+type HomeProps = {
+  product: {
+    priceId: string
+    amount: number
+  }
+}
+
+export default function Home ({ product }: HomeProps) {
   return (
     <>
       <Head>
         <title>Home | News</title>
       </Head>
 
-      <HomeTemplate />
+      <HomeTemplate product={ product }/>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JZUwuDBIo7Ry3hNLlSeXRlQ', {
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: formatCurrency({
+      value: price.unit_amount / 100,
+      locale: 'en-US',
+      currency: 'USD'
+    })
+  }
+
+  return {
+    props: {
+      product
+    }
+  }
 }
