84bf8587b435af36df73df9edcf1d5ccb3cd27b0
Gui-dev <adrianon10@gmail.com>
2021-09-16 02:55:01 -0300

    feat: add helper function handleSubscribe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,7 @@
+import { signIn, useSession } from 'next-auth/client'
+
+import { api } from 'services/api'
+import { getStripeJS } from 'services/stripe-js'
 import styles from './style.module.scss'
 
 type SubscribeButtonProps = {
@@ -5,8 +9,30 @@ type SubscribeButtonProps = {
 }
 
 export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
+  const [session] = useSession()
+
+  const handleSubscribe = async () => {
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    try {
+      const { data } = await api.post('/subscribe')
+      const { sessionId } = data
+      const stripe = await getStripeJS()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
   return (
-    <button className={ styles.subscribeButton }>
+    <button
+      className={ styles.subscribeButton }
+      onClick={ handleSubscribe }
+    >
       Subscribe now
     </button>
   )
