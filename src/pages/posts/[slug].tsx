34193c575f6c42e8b1ecbf8aa0b6bc00bbeb14dc
Gui-dev <adrianon10@gmail.com>
2021-09-21 18:46:36 -0300

    fix: fix name error

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -25,7 +25,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
   const session = await getSession({ req })
   const { slug } = params
 
-  if (!session?.activeSuscription) {
+  if (!session?.activeSubscription) {
     return {
       redirect: {
         destination: '/',
