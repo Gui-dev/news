7563fd99b45c35153066e92a5f92eab70bd73889
Gui-dev <adrianon10@gmail.com>
2021-11-30 03:10:16 -0300

    test: it should active link renders correctly

---
+++ src/components/ActiveLink/__tests__/ActiveLink.spec.tsx
@@ -0,0 +1,25 @@
+import { render } from '@testing-library/react'
+
+import { ActiveLink } from '..'
+
+jest.mock('next/router', () => {
+  return {
+    useRouter () {
+      return {
+        asPath: '/'
+      }
+    }
+  }
+})
+
+describe('<ActiveLink />', () => {
+  it('active link renders correctly', () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    )
+
+    expect(getByText('Home')).toBeInTheDocument()
+  })
+})
