1906a637c422d56bd7c4a34a87302a547ae39a32
Gui-dev <adrianon10@gmail.com>
2021-11-30 04:04:12 -0300

    test: it should component renders correctly when user is authenticated

--- src/components/SignInButton/__tests__/SignInButton.spec.tsx
+++ src/components/SignInButton/__tests__/SignInButton.spec.tsx
@@ -1,19 +1,39 @@
 import { render, screen } from '@testing-library/react'
+import { useSession } from 'next-auth/client'
+import { mocked } from 'ts-jest/utils'
 
 import { SignInButton } from '..'
 
-jest.mock('next-auth/client', () => {
-  return {
-    useSession () {
-      return [null, false]
-    }
-  }
-})
+jest.mock('next-auth/client')
 
 describe('<SignInButton />', () => {
   it('should component renders correctly when user is not authenticated', () => {
+    const useSeesionMocked = mocked(useSession)
+
+    useSeesionMocked.mockReturnValueOnce([null, false])
+
     render(<SignInButton />)
 
     expect(screen.getByText('Sign in with Github')).toBeInTheDocument()
+  })
+
+  it('should component renders correctly when user is authenticated', () => {
+    const useSeesionMocked = mocked(useSession)
+
+    useSeesionMocked.mockReturnValueOnce([
+      {
+        user: {
+          name: 'Bruce Wayne',
+          email: 'bruce@email.com',
+          image: 'image.jpg'
+        },
+        expires: 'fake-expires'
+      },
+      false
+    ])
+
+    render(<SignInButton />)
+
+    expect(screen.getByText('Bruce Wayne')).toBeInTheDocument()
   })
 })
