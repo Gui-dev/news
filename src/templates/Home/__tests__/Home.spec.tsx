f9b50dec11e2bf761ce58aaffaaae05c133eb660
Gui-dev <adrianon10@gmail.com>
2021-12-02 03:39:03 -0300

    test: it should loads initial datas

--- src/templates/Home/__tests__/Home.spec.tsx
+++ src/templates/Home/__tests__/Home.spec.tsx
@@ -1,6 +1,9 @@
 import { render, screen } from '@testing-library/react'
+import { mocked } from 'ts-jest/utils'
 
+import { stripe } from '../../../services/stripe'
 import { HomeTemplate } from '..'
+import { getStaticProps } from '../../../pages'
 
 jest.mock('next/router')
 jest.mock('next-auth/client', () => {
@@ -8,11 +11,33 @@ jest.mock('next-auth/client', () => {
     useSession: () => [null, false]
   }
 })
+jest.mock('../../../services/stripe')
 
 describe('<HomeTemplate />', () => {
   it('should renders correctly', () => {
     render(<HomeTemplate product={{ priceId: 'fake-price-id', amount: 'R$:10,00' }}/>)
 
     expect(screen.getByText(/R\$:10,00/i)).toBeInTheDocument()
+  })
+
+  it('should loads initial datas', async () => {
+    const stripPricesRetriveMocked = mocked(stripe.prices.retrieve)
+    stripPricesRetriveMocked.mockResolvedValueOnce({
+      id: 'fake-price-id',
+      unit_amount: 1000
+    } as any)
+
+    const response = await getStaticProps({})
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          product: {
+            priceId: 'fake-price-id',
+            amount: '$10.00'
+          }
+        }
+      })
+    )
   })
 })
