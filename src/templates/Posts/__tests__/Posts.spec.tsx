61349e997d40df4de3d62d2f55eaa3c6d0d46e62
Gui-dev <adrianon10@gmail.com>
2021-12-02 04:11:21 -0300

    test: it should loads initial datas

--- src/templates/Posts/__tests__/Posts.spec.tsx
+++ src/templates/Posts/__tests__/Posts.spec.tsx
@@ -1,22 +1,60 @@
 import { render, screen } from '@testing-library/react'
-// import { mocked } from 'ts-jest/utils'
+import { mocked } from 'ts-jest/utils'
 
 import { PostsTemplate } from '..'
-// import { getStaticProps } from '../../../pages/posts'
+import { getStaticProps } from '../../../pages/posts'
+import { getPrismicClient } from '../../../services/prismic'
 
 const posts = [
   {
-    slug: 'fake-slug',
+    slug: 'fake-post',
     title: 'fake-title',
     excert: 'fake-excerpt',
-    updatedAt: 'fake-date'
+    updatedAt: '12-02-2021'
   }
 ]
+
+jest.mock('../../../services/prismic')
 
 describe('<PostTemplate />', () => {
   it('should renders correctly', () => {
     render(<PostsTemplate posts={posts}/>)
 
     expect(screen.getByText(/fake-title/i)).toBeInTheDocument()
+  })
+
+  it('should loads initial datas', async () => {
+    const getPrismicClientMocked = mocked(getPrismicClient)
+    getPrismicClientMocked.mockReturnValueOnce({
+      query: jest.fn().mockResolvedValueOnce({
+        results: [
+          {
+            uid: 'fake-post',
+            data: {
+              title: [{ type: 'heading', text: 'fake-title' }],
+              content: [{ type: 'paragraph', text: 'fake-excerpt' }]
+            },
+            last_publication_date: '12-02-2021'
+          }
+        ]
+      })
+    } as any)
+
+    const response = await getStaticProps({})
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          posts: [
+            {
+              slug: 'fake-post',
+              title: 'fake-title',
+              excert: 'fake-excerpt',
+              updatedAt: '02 de dezembro de 2021'
+            }
+          ]
+        }
+      })
+    )
   })
 })
