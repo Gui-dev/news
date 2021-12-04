93f57b7fe2ac7fbbf8dd97e723d223c2d569bdc6
Gui-dev <adrianon10@gmail.com>
2021-12-04 03:26:37 -0300

    test: it should loads initial datas

--- src/templates/PostPreview/__tests__/PostPreview.spec.tsx
+++ src/templates/PostPreview/__tests__/PostPreview.spec.tsx
@@ -3,8 +3,8 @@ import { useSession } from 'next-auth/client'
 import { useRouter } from 'next/router'
 import { mocked } from 'ts-jest/utils'
 
-import PostPreview from '../../../pages/posts/preview/[slug]'
-// import { getPrismicClient } from '../../../services/prismic'
+import PostPreview, { getStaticProps } from '../../../pages/posts/preview/[slug]'
+import { getPrismicClient } from '../../../services/prismic'
 
 const post = {
   slug: 'fake-post',
@@ -47,5 +47,38 @@ describe('<PostPreviewTemplate />', () => {
     render(<PostPreview post={ post }/>)
 
     expect(pushMock).toHaveBeenCalledWith('/posts/fake-post')
+  })
+
+  it('should loads initial datas', async () => {
+    const getPrismicClientMocked = mocked(getPrismicClient)
+
+    getPrismicClientMocked.mockReturnValueOnce({
+      getByUID: jest.fn().mockResolvedValueOnce({
+        data: {
+          title: [{ type: 'heading', text: 'fake-title' }],
+          content: [{ type: 'paragraph', text: 'fake-content' }]
+        },
+        last_publication_date: '12-03-2021'
+      })
+    } as any)
+
+    const response = await getStaticProps({
+      params: {
+        slug: 'fake-post'
+      }
+    })
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          post: {
+            slug: 'fake-post',
+            title: 'fake-title',
+            content: '<p>fake-content</p>',
+            updatedAt: '03 de dezembro de 2021'
+          }
+        }
+      })
+    )
   })
 })
